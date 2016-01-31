28f4748ee20eb8d14ee5dbac6f75db71c5b9e35e
Keil Hunsaker <keilhun@gmail.com>
2016-01-30 20:00:27 -0500

    Completed step 5 in exercise

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,5 +1,6 @@
 require 'jumpstart_auth'
-
+require 'bitly'
+Bitly.use_api_version_3
 class MicroBlogger
   attr_reader :client
   
@@ -42,12 +43,20 @@ class MicroBlogger
   end
   
   def everyones_last_tweet
-    friends = @client.friends.collect { |friend| @client.user(friend) }
+    friends = @client.friends.collect { |friend| @client.user(friend) }.sort_by {|friend| friend.screen_name.downcase}
     friends.each do |friend|
-      puts friend.screen_name
+     # puts friend.screen_name
+      timestamp = friend.status.created_at
+      printf("%s said this on %s\n", friend.screen_name, timestamp.strftime("%A, %b %d"))
       puts friend.status.text
       puts ""
     end
+  end
+  
+  def shorten(original_url)
+    bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    return bitly.shorten(original_url).short_url
+    #puts "Shortening this URL: #{original_url}"
   end
   
   def run
@@ -65,6 +74,8 @@ class MicroBlogger
         when 'dm' then dm(parts[1], parts[2..-1].join(" "))
         when 'spam' then spam_my_followers(parts[1..-1].join(" "))
         when 'elt' then everyones_last_tweet()
+        when 's' then shorten(parts[1])
+        when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
         else
           puts "Sorry, I don't how to #{command}"
       end
